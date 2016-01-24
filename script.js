43136ba0a9068fdba71bc4f0cff8725e7c5da00c
Kevin Lee <LKevinLeeL@gmail.com>
2016-01-23 20:01:35 -0500

    rebrand

--- script.js
+++ script.js
@@ -67,3 +67,45 @@ function validate(){
         $("input[type=submit]").prop("disabled", true);
     }
 }
+
+
+
+
+////////////////////
+// COUNTDOWN
+///////////////////
+CountDownTimer('03/26/2016 9:00 AM', 'countdown'); //Month.Day
+
+    function CountDownTimer(dt, id)
+    {
+        var end = new Date(dt);
+
+        var _second = 1000;
+        var _minute = _second * 60;
+        var _hour = _minute * 60;
+        var _day = _hour * 24;
+        var timer;
+
+        function showRemaining() {
+            var now = new Date();
+            var distance = end - now;
+            if (distance < 0) {
+
+                clearInterval(timer);
+                document.getElementById(id).innerHTML = 'Too Late! Next time.';
+
+                return;
+            }
+            var days = Math.floor(distance / _day);
+            var hours = Math.floor((distance % _day) / _hour);
+            var minutes = Math.floor((distance % _hour) / _minute);
+            var seconds = Math.floor((distance % _minute) / _second);
+
+            document.getElementById(id).innerHTML = days + '&nbsp;'+ 'Days ';
+            document.getElementById(id).innerHTML += hours + '&nbsp;'+'Hrs ';
+            document.getElementById(id).innerHTML += minutes + '&nbsp;'+'Mins ';
+            document.getElementById(id).innerHTML += seconds + '&nbsp;'+'Secs';
+        }
+
+        timer = setInterval(showRemaining, 1000);
+    }
